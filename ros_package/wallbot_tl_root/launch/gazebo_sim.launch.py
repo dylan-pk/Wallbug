e3e315a7d97a2da524a92afafecc6df11e94aec6
Connor <cajwilliams13@gmail.com>
2025-11-10 22:49:25 +1100

    Migration to Gazebo Fortress (Gazebo Ignition 6)

--- ros_package/wallbot_tl_root/launch/gazebo_sim.launch.py
+++ ros_package/wallbot_tl_root/launch/gazebo_sim.launch.py
@@ -5,12 +5,18 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import AppendEnvironmentVariable
 
 from launch_ros.actions import Node
 
 def generate_launch_description():
 
     package_name = 'wallbot_tl_root'
+
+    set_env_vars_resources = AppendEnvironmentVariable(
+        'GZ_SIM_RESOURCE_PATH',
+        os.path.join(get_package_share_directory(package_name),
+                     'models'))
 
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
@@ -19,14 +25,14 @@ def generate_launch_description():
     )
 
     gazebo = IncludeLaunchDescription(PythonLaunchDescriptionSource(
-        [os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+        [os.path.join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
         launch_arguments={
-            'world': os.path.join(get_package_share_directory(package_name), 'world', 'wallbot_world.world')   # optional: start paused
+            'gz_args': ['-r ', os.path.join(get_package_share_directory(package_name), 'world', 'wallbot_world.world')]   # optional: start paused
             }.items())
     
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+    spawn_entity = Node(package='ros_gz_sim', executable='create',
                         arguments=['-topic', 'robot_description',
-                                   '-entity', 'wallbot',
+                                   '-name', 'wallbot',
                                    '-x', '0.0', '-y', '0.0', '-z', '3'])
     
     joint_broad_spawner = Node(
@@ -46,6 +52,7 @@ def generate_launch_description():
     return LaunchDescription([
         rsp,
         gazebo,
+        set_env_vars_resources,
         spawn_entity,
         joint_traj_cont_spawner,
         joint_broad_spawner
